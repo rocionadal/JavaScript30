f9285d1dc53644d27d3a1c3576cdb5e378c8fe2e
rocionadal <rocioonadal@gmail.com>
2021-01-31 17:28:15 -0300

    displaying and graving en time

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,4 +1,7 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
   // clear any existing timers
@@ -21,6 +24,14 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
-  const minutes = seconds / 60;
-  console.log(seconds);
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
 }
